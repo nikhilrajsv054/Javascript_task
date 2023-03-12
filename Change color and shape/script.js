ff140fa102d8611844a7e89d301e59bc42c0737c
nikhilrajsv054 <svnikhilraj2a@gmail.com>
2023-03-12 07:39:50 +0530

    ChangeColorAndShape

--- Change color and shape/script.js
+++ Change color and shape/script.js
@@ -30,4 +30,15 @@ function changeShape()
     i.className = "inner";
     isTriangle = false;
   }
+
+    // var c = document.getElementById("inner");//document.getElementById("myCanvas");
+    // var ctx = c.getContext("2d");
+    // ctx.beginPath();
+    // ctx.moveTo(20, 20);
+    // ctx.lineTo(20, 100);
+    // ctx.lineTo(70, 100);
+    // ctx.closePath();
+    // ctx.stroke();
+    // ctx.fillStyle = "red";
+    // ctx.fill();
 }
